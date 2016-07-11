d76c92e7fdbd1c8733293f651891f5990f930565
TuxCrafting <elie@agriates.com>
2016-07-11 22:05:43 +0200

    thx @Downgoat

--- yourmom.js
+++ yourmom.js
@@ -5,13 +5,13 @@ var fs = require("fs");
 var args = process.argv.splice(2);
 
 if (!args[0]) {
-    console.log("y u do dis ;_; y u forgoten the filename ;_;");
+    console.log(";_; u fergit file name ;_; y u do dis");
     process.exit(1);
 }
 
 fs.readFile(args[0], "utf-8", (err, data) => {
     if (err) {
-        console.log("y u do dis ;_; y u given me a unexistant file ;_;");
+        console.log("y u do dis ;_; y u given me a unexistant file ಠ_ಠ");
         process.exit(1);
     }
 });
