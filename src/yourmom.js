51b7ccc56b786afd10bbd0f2651316de6809c54f
TuxCrafting <elie@agriates.com>
2016-07-12 14:07:00 +0200

    TURING COMPLETENESS (at least i think ;_;)

--- src/yourmom.js
+++ src/yourmom.js
@@ -1,37 +1,56 @@
 "use strict";
 
-/* UNICODE SUBSTITUTIONS: */
-/* i am 2 lazy google it */
+const readlineSync = require("readline-sync");
+const fs = require("fs");
 
-var NUMS = "0123456789";
+const args = process.argv.splice(2);
+
+const NUMS = "0123456789ABCDEFGHIJKLMN";
 
 class YourMom {
     constructor() {
         this.stack = [];
+        this.backmemory = [];
         this.memory = [];
     }
     pop() {
         if (this.stack.length) return this.stack.pop();
+        return this.readv();
+    }
+    readv() {
+        var _ = readlineSync.prompt({ prompt: "" });
+        if (_.match(/^[-+]?\d+$/g) != null) return parseInt(_);
+        if (_.match(/^[-+]?\d+(\.\d*)?(e[-+]?\d+(\.\d+)?)?$/g) != null) return parseFloat(_);
+        return _;
     }
     run(tokens) {
+        var s, _;
         while (tokens.length) {
             var tok = tokens.shift();
             if (NUMS.indexOf(tok) != -1)
-                this.stack.push(this.pop() * 10 + (tok.charCodeAt(0) - 0x30));
+                this.stack.push(this.pop() * 24 + NUMS.indexOf(tok));
             else switch (tok) {
             case "+": this.applydyadic((a, b) => a + b); break;
             case "-": this.applydyadic((a, b) => a - b); break;
             case "*": this.applydyadic((a, b) => a * b); break;
             case "/": this.applydyadic((a, b) => a / b); break;
             case "%": this.applydyadic((a, b) => a % b); break;
+            case "=": this.applydyadic((a, b) => +(a == b)); break;
+            case "<": this.applydyadic((a, b) => +(a < b)); break;
+            case ">": this.applydyadic((a, b) => +(a > b)); break;
             case "#": this.stack.push(0); break;
+            case "¥": this.stack.push(1); break;
+            case "&": this.applydyadic((a, b) => a & b); break;
+            case "|": this.applydyadic((a, b) => a | b); break;
+            case "^": this.applydyadic((a, b) => a ^ b); break;
+            case "_": console.log(this.pop()); break;
             case "!":
                 var i = this.pop();
                 this.memory[i] = this.pop();
                 break;
             case "?": this.stack.push(this.memory[this.pop()]); break;
             case "'":
-                var s = "";
+                s = "";
                 while (tokens.length) {
                     tok = tokens.shift();
                     if (tok == "'") break;
@@ -39,14 +58,47 @@ class YourMom {
                 }
                 this.stack.push(s);
                 break;
-            case "\u229E": this.stack.push(this.pop() + 1); break;
-            case "\u229F": this.stack.push(this.pop() - 1); break;
+            case "⊞": this.stack.push(this.pop() + 1); break;
+            case "⊟": this.stack.push(this.pop() - 1); break;
             case ":":
-                var tmp = this.pop();
-                this.stack.push(tmp);
-                this.stack.push(tmp);
+                _ = this.pop();
+                this.stack.push(_);
+                this.stack.push(_);
                 break;
             case ";": this.pop(); break;
+            case "(":
+                s = "";
+                var sm = false;
+                var l = 0;
+                while (tokens.length) {
+                    tok = tokens.shift();
+                    if (tok == "'") sm = !sm;
+                    if (!sm) {
+                        if (tok == "(") l++;
+                        else if (tok == ")") {
+                            if (l) l--;
+                            else break;
+                        }
+                    }
+                    s += tok;
+                }
+                this.stack.push(() => {
+                    this.run(s.split(""));
+                });
+                break;
+            case "$": this.pop()(); break;
+            case "\\":
+                _ = this.memory;
+                this.memory = this.backmemory;
+                this.backmemory = _;
+                break;
+            case "@":
+                _ = this.pop();
+                while (this.stack[this.stack.length - 1]) _();
+                break;
+            case "ð": this.stack.push(readlineSync.prompt({ prompt: "" })); break;
+            case "Ð": this.stack.push(this.readv()); break;
+            case "©": console.log(this.pop()); break;
             }
         }
     }
@@ -56,10 +108,6 @@ class YourMom {
         this.stack.push(l(b, a));
     }
 }
-
-var fs = require("fs");
-
-var args = process.argv.splice(2);
 
 if (!args[0]) {
     console.log(";_; u fergit file name ;_; y u do dis");
