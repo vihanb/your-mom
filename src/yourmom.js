18e408c93d225ed5aeb8765c32844293f1ccffd9
TuxCrafting <elie@agriates.com>
2016-07-13 21:47:29 +0200

    Added commander and preparing posting on NPM

--- src/yourmom.js
+++ src/yourmom.js
@@ -2,10 +2,9 @@
 
 const prompt = require("prompt-sync")();
 const fs = require("fs");
+const commander = require("commander");
 require("./utils.js");
 const jellycomp = require("./jellycomp.js");
-
-const args = process.argv.splice(2);
 
 const NUMS = "0123456789ABCDEFGHIJKLMN";
 const WHITESPACE = " \n\t\v\r\f";
@@ -196,17 +195,26 @@ class YourMom {
     }
 }
 
-if (!args[0]) {
-    console.log(";_; u fergit file name ;_; y u do dis");
-    process.exit(1);
+commander
+    .version("1.0.0")
+    .usage("[options] <file>")
+    .option("-e, --idkhowtocallit", "execute program from argv")
+    .parse(process.argv);
+
+function _run(data) {
+    let ym = new YourMom();
+    ym.run([...data]);
+    while (ym.stack.length) console.log(ym.stack.pop());
 }
 
-fs.readFile(args[0], "utf-8", (err, data) => {
-    if (err) {
-        console.log("y u do dis ;_; y u given me a unexistant file \u0CA0_\u0CA0");
-        process.exit(1);
-    }
-    var t = new YourMom();
-    t.run([...data]);
-    while (t.stack.length) console.log(t.stack.pop());
-});
+if (commander.idkhowtocallit) {
+    _run(commander.args[0]);
+} else {
+    fs.readFile(commander.args[0], "utf-8", (err, data) => {
+        if (err) {
+            console.log("y u do dis ;_; y u given me a unexistant file \u0CA0_\u0CA0");
+            process.exit(1);
+        }
+        _run(data);
+    });
+}
